f6da915867a2ed153502757ed770376b00312734
Svatosik <125908175+Svatosik@users.noreply.github.com>
2023-07-28 23:01:16 +0200

    v2(task1-3)

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const list = document.querySelector("#categories");
+const itemsList = list.querySelectorAll(".item");
+
+console.log("Number of categories:", itemsList.length);
+itemsList.forEach((item) => {
+  const categoryTitle = item.firstElementChild.textContent;
+  const categoryItem = item.lastElementChild.children.length;
+
+  console.log("\nCategory:", categoryTitle);
+  console.log("Elements", categoryItem);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const ingredientsElements = ingredients.map((ingredient) => {
+  const ingredientsItem = document.createElement("li");
+  ingredientsItem.textContent = ingredient;
+  ingredientsItem.classList.add("item");
+  return ingredientsItem;
+})
+
+ingredientsList.append(...ingredientsElements);
